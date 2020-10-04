c0f195658f50a1b1e2198a01447a54c3d8d46c62
stevewb1993 <stevewbowser@gmail.com>
2020-10-04 15:12:01 +0100

    added function for loading spark config

--- src/main/scala/com/stevebowser/enduranceactivityfileanalyser/Analyser.scala
+++ src/main/scala/com/stevebowser/enduranceactivityfileanalyser/Analyser.scala
@@ -1,15 +1,19 @@
 package com.stevebowser.enduranceactivityfileanalyser
 
+import java.util.Properties
+
 import com.stevebowser.enduranceactivityfileanalyser.fileparser.FileParser
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.{Dataset, SparkSession}
+
+import scala.io.Source
 
 object Analyser {
 
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession.builder()
-      .appName("GPXAnalyser")
-      .master("local[3]")
+      .config(getSparkAppConf)
       .getOrCreate()
 
     val testActivityDataset : Dataset[FileParser.ActivityRecord] = FileParser.readGPXToDataFrame("Data/activity_4900763877.gpx", spark)
@@ -19,4 +23,14 @@ object Analyser {
     spark.stop()
 
   }
+
+  def getSparkAppConf: SparkConf = {
+    val sparkAppConf = new SparkConf
+    //Set all Spark Configs
+    val props = new Properties
+    props.load(Source.fromFile("spark.conf").bufferedReader())
+    props.forEach((k, v) => sparkAppConf.set(k.toString, v.toString))
+    sparkAppConf
+  }
+
 }
